2f9c5bcf223dd797c7a62c0967e2deb70f2c848e
Nazareno Barnard <nazarenob2501@gmail.com>
2022-10-17 17:41:01 -0300

    confirmacion

--- SABOR A CASA PROJECTO/models/bd.js
+++ SABOR A CASA PROJECTO/models/bd.js
@@ -1,16 +1,16 @@
-var mysql =requiere('mysql');
-var util = requiere('util');
+// var mysql =requiere('mysql');
+// var util = requiere('util');
 
-var pool = mysql.createPool({
-    connectionLimit: 10,
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    datebase: process.env.MYSQL_DB_NAME
+// var pool = mysql.createPool({
+//     connectionLimit: 10,
+//     host: process.env.MYSQL_HOST,
+//     user: process.env.MYSQL_USER,
+//     password: process.env.MYSQL_PASSWORD,
+//     datebase: process.env.MYSQL_DB_NAME
 
-});
+// });
  
 
-pool.query = util.pronisify(pool.query);
+// pool.query = util.pronisify(pool.query);
 
-module.exports = pool;
+// module.exports = pool;
