5b2794a84cb7ca021d2dcd037e2ebacb75bf1720
Nazareno Barnard <nazarenob2501@gmail.com>
2022-10-18 17:39:28 -0300

    main

--- SABOR A CASA PROJECTO/models/usuariosModel.js
+++ SABOR A CASA PROJECTO/models/usuariosModel.js
@@ -1,14 +1,15 @@
-// var pool = requiere('./bd');
-// var md5 = requiere('md5');
+var pool = require('./bd');
+var md5 = require('md5');
 
-// async function getUserAndPassword(user, password) {
-//     try {
-//         var query = 'select * from usuarios where usuario = ? and password = ? limit 1';
-//         var rows = await pool.query(query, [user, md5 (password)]);
-//         return rows[0];
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
+async function getUserPassword (user, password) {
+    try {
+        var query = 'select * from usuarios where usuario = ? and password = ? limit 1';
+        var rows = await pool.query(query, [user, md5(password)]);
+        return rows[0];
 
-// module.exports = { getUserAndPassword }
+ } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = {getUserPassword} 
