4eaaccf4dd011e6d318ea007d982a71bffe337f1
Nazareno Barnard <nazarenob2501@gmail.com>
2022-10-25 12:38:19 -0300

    confrimacion

--- SABOR A CASA PROJECTO/routes/admin/novedades.js
+++ SABOR A CASA PROJECTO/routes/admin/novedades.js
@@ -24,4 +24,69 @@ router.get('/eliminar/:id', async (req, res, next) => {
 /* para eliminar novedad */
 
 
+/* para agregar novedad */
+
+router.get('/agregar', (req, res, next) => {
+    res.render('admin/agregar', {
+        layout: 'admin/layout'
+    })
+});
+/* para agregar novedad */
+
+router.post('/agregar', async (req, res, next) =>{
+    try {
+        // console.log(req.body)
+        if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") 
+        {
+            await novedadesModel.insertNovedad(req.body);
+            res.redirect('/admin/novedades')
+        } else {
+            res.render('admin/agregar',  {
+                layout: 'admin/layout', 
+                error: true,
+                massage: 'Todos los campos son requeridos'
+            })
+        }
+} catch (error)  {
+    console.log(error)
+    res.render('admin/agregar',  {
+        layout: 'admin/layout',
+        error: true,
+        message: 'No se cargo la novedad'
+      })
+   } 
+})
+
+router.get('/modificar/:id', async (req, res, next) => {
+    var id = req.params.id;
+    var novedad = await novedadesModel.getNovedadesById(id);
+    res.render("admin/modificar" , {
+      layout: 'admin/layout',
+      novedad
+    });
+});
+
+
+router.post('/modificar', async (req, res, next) => {
+    try {
+        var obj = {
+            titulo: req.body.titulo,
+            cuerpo: req.body.cuerpo
+        }
+
+        console.log(obj) 
+        await novedadesModel.modificarNovedadById(obj, req.body.id);
+        res.redirect('/admin/novedades');
+    } catch (error) {
+        console.log(error)
+        res.render('admin/modificar', {
+            layout: 'admin/layout',
+            error: true,
+            massage: 'No se modifico la noverdad'
+        })
+    }
+});
+
+
+
 module.exports = router;
